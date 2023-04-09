ea3cfcebb437c95cad89f4314de066675744a686
lbialczak <lbialczak@grupazpr.pl>
2023-04-09 13:23:46 +0200

    var table

--- src/main/java/ats/v1/pkb/var_table/VarTableImpl.java
+++ src/main/java/ats/v1/pkb/var_table/VarTableImpl.java
@@ -1,4 +1,27 @@
 package ats.v1.pkb.var_table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VarTableImpl implements VarTable {
+    List<String> table = new ArrayList<>();
+
+    @Override
+    public int insert(String name) {
+        if (table.contains(name))
+            return -1;
+
+        table.add(name);
+        return table.indexOf(name);
+    }
+
+    @Override
+    public String getName(int idx) {
+        return table.get(idx);
+    }
+
+    @Override
+    public int getIndexOf(String name) {
+        return table.indexOf(name);
+    }
 }
