e241b00965dc06c505da4ce84099d4065b84ab4b
lbialczak <lbialczak@grupazpr.pl>
2023-04-29 22:41:42 +0200

    Node modifications

--- src/main/java/ats/v1/pkb/ast/nodes/AssignNode.java
+++ src/main/java/ats/v1/pkb/ast/nodes/AssignNode.java
@@ -1,5 +1,7 @@
 package ats.v1.pkb.ast.nodes;
 
-public class AssignNode extends Node {
-
+public class AssignNode extends StatementNode {
+    public AssignNode(int line) {
+        super(line);
+    }
 }
