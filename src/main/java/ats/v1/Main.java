d869e1b27e54bf77733a0562c26610216d62cd77
kowalesiu <m.kowalewski001@gmail.com>
2023-05-09 21:24:41 +0200

    Implementacja Follows oraz naprawa pom oraz jar

--- src/main/java/ats/v1/Main.java
+++ src/main/java/ats/v1/Main.java
@@ -2,26 +2,23 @@ package ats.v1;
 
 import ats.v1.pkb.Parser;
 import ats.v1.pkb.ast.Ast;
-import ats.v1.pkb.ast.nodes.Node;
 import ats.v1.pkb.proc_table.ProcTable;
 import ats.v1.pkb.proc_table.ProcTableImpl;
 import ats.v1.pkb.var_table.VarTable;
 import ats.v1.pkb.var_table.VarTableImpl;
-import ats.v1.spa_frontend.token.Token;
 import ats.v1.spa_frontend.scanner.Scanner;
+import ats.v1.spa_frontend.token.Token;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+        log.info("Test");
         try {
             processFile(args[0]);
         } catch (Exception e) {
