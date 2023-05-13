8ff45f641775434de374957d334d8b426e14a184
PiotrS0 <piotr.sawczuk@10g.pl>
2023-05-13 09:25:30 +0200

    QueryCompositor in progress

--- src/main/java/ats/v1/query/processor/QueryNode.java
+++ src/main/java/ats/v1/query/processor/QueryNode.java
@@ -1,9 +1,11 @@
 package ats.v1.query.processor;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 @AllArgsConstructor
 public class QueryNode {
 
@@ -13,6 +15,11 @@ public class QueryNode {
 
     private final List<QueryNode> children;
 
+    public QueryNode(){
+        this.name = null;
+        this.parent = null;
+        this.children = null;
+    }
     public QueryNode(final String name) {
         this.name = name;
         this.parent = null;
