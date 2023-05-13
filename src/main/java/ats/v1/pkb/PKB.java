f56ec2a1b67d283d6e333c6bf5a565beca9a34e8
lbialczak <lbialczak@grupazpr.pl>
2023-05-13 11:52:06 +0200

    take types as input and filter results

--- src/main/java/ats/v1/pkb/PKB.java
+++ src/main/java/ats/v1/pkb/PKB.java
@@ -1,15 +1,23 @@
 package ats.v1.pkb;
 
+import ats.v1.pkb.ast.nodes.AssignNode;
 import ats.v1.pkb.ast.nodes.Node;
 import ats.v1.pkb.ast.nodes.StatementNode;
+import ats.v1.pkb.ast.nodes.WhileNode;
 import ats.v1.pkb.modifies_table.ModifiesTable;
 import ats.v1.pkb.statement_table.StatementTable;
 import ats.v1.pkb.uses_table.UsesTable;
 import ats.v1.pkb.var_table.VarTable;
+import ats.v1.spa_frontend.token.TokenType;
 import lombok.AllArgsConstructor;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static ats.v1.spa_frontend.token.TokenType.PROCEDURE;
+import static ats.v1.spa_frontend.token.TokenType.WHILE;
 
 @AllArgsConstructor
 public class PKB {
@@ -17,6 +25,13 @@ public class PKB {
     private UsesTable utable;
     private VarTable varTable;
     private StatementTable statTable;
+
+    private static final Map<String, Class> statementMapping;
+    static {
+        statementMapping = new HashMap<>();
+        statementMapping.put("a", AssignNode.class);
+        statementMapping.put("w", WhileNode.class);
+    } //TODO wynieść do zewnętrznej klasy
 
     public boolean modifies(int statement, String var) {
         return mtable.isModified(varTable.getIndexOf(var), statement);
@@ -27,8 +42,9 @@ public class PKB {
         return idxList.stream().map(i -> varTable.getName(i)).collect(Collectors.toList());
     }
 
-    public List<Integer> modifies(String var) {
-        return mtable.getModifies(varTable.getIndexOf(var));
+    public List<Integer> modifies(String var, String type) {
+        List<Integer> allStatements = mtable.getModifies(varTable.getIndexOf(var));
+        return filterStatements(allStatements, type);
     }
 
     public boolean uses(int statement, String var) {
@@ -40,8 +56,9 @@ public class PKB {
         return idxList.stream().map(i -> varTable.getName(i)).collect(Collectors.toList());
     }
 
-    public List<Integer> uses(String var) {
-        return utable.getUses(varTable.getIndexOf(var));
+    public List<Integer> uses(String var, String type) {
+        List<Integer> allStatements = utable.getUses(varTable.getIndexOf(var));
+        return filterStatements(allStatements, type);
     }
 
     public boolean follows(int s1, int s2) {
@@ -70,4 +87,14 @@ public class PKB {
         return ((StatementNode)before).getLine();
     }
 
+
+    public List<Integer> filterStatements(List<Integer> all, String type) {
+        if (type == null) {
+            return all;
+        }
+        return all
+                .stream()
+                .filter(stmt -> statTable.getStatement(stmt).getClass() == statementMapping.get(type))
+                .collect(Collectors.toList());
+    }
 }
